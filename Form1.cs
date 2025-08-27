28f59fe3694b001279c1a52ea3298e2ed7378f9e
kimjihye058 <s2321@e-mirim.hs.kr>
2025-08-27 09:40:15 +0900

    #6 운세 가져오는 메서드 구현

--- Form1.cs
+++ Form1.cs
@@ -57,6 +57,13 @@ namespace FortuneTeller
             }
         }
 
+        private string GetFortune()
+        {
+            Random random = new Random();
+            int index = random.Next(0, results.Count);
+            return results[index];
+        }
+
         private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
